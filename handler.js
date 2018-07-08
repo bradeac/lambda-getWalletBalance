66978cb9026323fd57fef9ce3bf490449f9dbe82
bradeac <rs6avant>
2018-07-08 23:10:55 +0300

    Implemented more generic and better instantiation of an exchange

--- handler.js
+++ handler.js
@@ -1,9 +1,10 @@
 import ccxt from 'ccxt'
 
 export const getWalletBalance = async (event, context, callback) => {
-    const { exchange, coin } = event.params.path || { exchange: null, coin: null }
+    const pathParams = event.params ? event.params.path : {}
+	const { exchange, coin } = pathParams
 
-    if (exchange && coin) {
+    if (exchange && coin && ccxt.exchanges.indexOf(exchange) > -1) {
         const response = await get(exchange, coin)
 
         callback(null, response)
@@ -53,48 +54,11 @@ const get = async (exchange, coin) => {
 }
 
 const instantiateExchange = (exchange) => {
-    let exchangeObj = null
+    const apiKeyName = `${exchange}_api_key`
+    const secretKeyName = `${exchange}_secret_key`
 
-    switch(exchange) {
-        case 'hitbtc': {
-            exchangeObj = new ccxt.hitbtc ({
-                'apiKey': process.env.hitbtc_api_key,
-                'secret': process.env.hitbtc_secret_key,
-            })
-
-            break
-        }
-
-        case 'binance': {
-            exchangeObj = new ccxt.binance ({
-                'apiKey': process.env.binance_api_key,
-                'secret': process.env.binance_secret_key,
-            })
-
-            break
-        }
-
-        case 'liqui': {
-            exchangeObj = new ccxt.liqui ({
-                'apiKey': process.env.liqui_api_key,
-                'secret': process.env.liqui_secret_key,
-            })
-
-            break
-        }
-
-        case 'kucoin': {
-            exchangeObj = new ccxt.kucoin ({
-                'apiKey': process.env.kucoin_api_key,
-                'secret': process.env.kucoin_secret_key,
-            })
-
-            break
-        }
-
-        default: 
-            break
-    }
-
-    return exchangeObj
+    return new ccxt[exchange]({ 
+        'apiKey': process.env[apiKeyName], 
+        'secret': process.env[secretKeyName], 
+    })
 }
